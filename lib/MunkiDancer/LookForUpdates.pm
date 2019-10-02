a3b729bebae127ebe869c11f54083b68893a1494
Claude Becker <becker@phys.ethz.ch>
2019-10-02 14:10:23 +0200

    adapt regexp for macupdate redesign

--- lib/MunkiDancer/LookForUpdates.pm
+++ lib/MunkiDancer/LookForUpdates.pm
@@ -35,10 +35,7 @@ sub LatestVersion {
     my $html = $mech->content || '';
 
     my $match_version_number = qr{
-        id="app_info_version_2"         # id used for version numbers
-        [^>]* >                         # closing bracket of that tag
-        \s*                             # ignore spaces and newlines
-        <a[^>]*>\s*([^<\s]*)\s*<\/a>    # extract version number from link
+        <span\sclass="mu_app_header_version">([^<]*)<\/span>
     }xi;
 
     (my $version) = $html =~ m/$match_version_number/;
